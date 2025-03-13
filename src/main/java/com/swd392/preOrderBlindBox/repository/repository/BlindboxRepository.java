1defe5f199073f913a8636cbcd6ad1bd5064f9b1
Dung Pham <anhdungp0304@gmail.com>
2025-03-13 14:32:02 +0700

    add missing repository annotations

--- src/main/java/com/swd392/preOrderBlindBox/repository/repository/BlindboxRepository.java
+++ src/main/java/com/swd392/preOrderBlindBox/repository/repository/BlindboxRepository.java
@@ -2,9 +2,11 @@ package com.swd392.preOrderBlindBox.repository.repository;
 
 import com.swd392.preOrderBlindBox.entity.Blindbox;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BlindboxRepository extends JpaRepository<Blindbox, Long> {
     List<Blindbox> findByBlindboxPackageId(Long packageId);
 }
